ab9f314c1f7b2a2cd515df4b8e73c0abe8f240ce
Jason Feimster <jasonfeimster@gmail.com>
2026-09-11 06:01:40 -0400

    Add Get-ProjectStatus script scaffold

---
+++ tools/Get-ProjectStatus/scripts/Get-ProjectStatus.ps1
@@ -0,0 +1,9 @@
+function Get-ProjectStatus {
+    [CmdletBinding()]
+    param(
+        [Parameter(Position=0)]
+        [string]$Repository = '.'
+    )
+
+    throw 'Get-ProjectStatus scaffold created; implementation pending.'
+}
