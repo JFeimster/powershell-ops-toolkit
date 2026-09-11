f0d0a62d631a80251e67e5260c3a57831910057b
Jason Feimster <jasonfeimster@gmail.com>
2026-09-11 06:02:35 -0400

    Add Get-ProjectLinks script scaffold

---
+++ tools/Get-ProjectLinks/scripts/Get-ProjectLinks.ps1
@@ -0,0 +1,9 @@
+function Get-ProjectLinks {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory, Position=0)]
+        [string]$Repository
+    )
+
+    throw 'Get-ProjectLinks scaffold created; implementation pending.'
+}
