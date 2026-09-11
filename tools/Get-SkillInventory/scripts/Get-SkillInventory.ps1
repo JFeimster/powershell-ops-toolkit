1b2b78ef4ea982336a7f7a7af2567aaf241b13c8
Jason Feimster <jasonfeimster@gmail.com>
2026-09-11 06:07:59 -0400

    Add Get-SkillInventory script scaffold

---
+++ tools/Get-SkillInventory/scripts/Get-SkillInventory.ps1
@@ -0,0 +1,8 @@
+function Get-SkillInventory {
+    [CmdletBinding()]
+    param(
+        [string]$SkillsRoot = 'C:\Users\jason\OneDrive\Desktop\JSON\skills'
+    )
+
+    throw 'Get-SkillInventory scaffold created; implementation pending.'
+}
