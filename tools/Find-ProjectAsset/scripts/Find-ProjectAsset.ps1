c7383a0c202f6521b061a9bcf7b6da6e0dad800b
Jason Feimster <jasonfeimster@gmail.com>
2026-09-11 06:03:46 -0400

    Add Find-ProjectAsset script scaffold

---
+++ tools/Find-ProjectAsset/scripts/Find-ProjectAsset.ps1
@@ -0,0 +1,9 @@
+function Find-ProjectAsset {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory, Position=0)]
+        [string]$Query
+    )
+
+    throw 'Find-ProjectAsset scaffold created; implementation pending.'
+}
