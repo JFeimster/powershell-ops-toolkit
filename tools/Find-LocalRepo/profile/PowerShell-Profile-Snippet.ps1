b2560a1f5578fe74338d86a8216b6e5845520323
Jason Feimster <jasonfeimster@gmail.com>
2026-09-11 05:22:58 -0400

    Delete tools/Find-LocalRepo directory

--- tools/Find-LocalRepo/profile/PowerShell-Profile-Snippet.ps1
+++
@@ -1,6 +0,0 @@
-# >>> local-repo-finder >>>
-$localRepoFinderScript = 'C:\Users\jason\OneDrive\Desktop\JSON\skills\local-repo-finder\scripts\Find-LocalRepo.ps1'
-if (Test-Path -LiteralPath $localRepoFinderScript) {
-    . $localRepoFinderScript
-}
-# <<< local-repo-finder <<<
