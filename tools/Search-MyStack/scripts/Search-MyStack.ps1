9c0a986a35ac6feadc9396b91b216bd973a28eff
Jason Feimster <jasonfeimster@gmail.com>
2026-09-11 06:08:16 -0400

    Add Search-MyStack script scaffold

---
+++ tools/Search-MyStack/scripts/Search-MyStack.ps1
@@ -0,0 +1,10 @@
+function Search-MyStack {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory, Position=0)]
+        [string]$Query,
+        [string[]]$Source
+    )
+
+    throw 'Search-MyStack scaffold created; implementation pending.'
+}
