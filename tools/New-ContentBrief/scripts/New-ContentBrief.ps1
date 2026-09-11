1306af704476d6ee5c3a1522b410140e6f88d62b
Jason Feimster <jasonfeimster@gmail.com>
2026-09-11 06:06:55 -0400

    Add New-ContentBrief script scaffold

---
+++ tools/New-ContentBrief/scripts/New-ContentBrief.ps1
@@ -0,0 +1,9 @@
+function New-ContentBrief {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory, Position=0)]
+        [string]$Topic
+    )
+
+    throw 'New-ContentBrief scaffold created; implementation pending.'
+}
