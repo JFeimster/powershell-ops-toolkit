aa490c20854c67980fa87fd1ba9ed1ce6fccf602
Jason Feimster <jasonfeimster@gmail.com>
2026-09-11 06:04:54 -0400

    Add Open-GitHubRepo script scaffold

---
+++ tools/Open-GitHubRepo/scripts/Open-GitHubRepo.ps1
@@ -0,0 +1,9 @@
+function Open-GitHubRepo {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory, Position=0)]
+        [string]$Repository
+    )
+
+    throw 'Open-GitHubRepo scaffold created; implementation pending.'
+}
