34a12d21296b4d764cdbe0bda85c80e898c3e02b
Jason Feimster <jasonfeimster@gmail.com>
2026-09-11 06:06:02 -0400

    Add Test-SiteRoutes script scaffold

---
+++ tools/Test-SiteRoutes/scripts/Test-SiteRoutes.ps1
@@ -0,0 +1,9 @@
+function Test-SiteRoutes {
+    [CmdletBinding()]
+    param(
+        [Parameter(Position=0)]
+        [string]$Repository = '.'
+    )
+
+    throw 'Test-SiteRoutes scaffold created; implementation pending.'
+}
