e0e64a1218ac8aaa4f959dc6c78445a5d8804221
Jason Feimster <jasonfeimster@gmail.com>
2026-09-11 06:04:22 -0400

    Add Get-ChangedFiles script scaffold

---
+++ tools/Get-ChangedFiles/scripts/Get-ChangedFiles.ps1
@@ -0,0 +1,9 @@
+function Get-ChangedFiles {
+    [CmdletBinding()]
+    param(
+        [Parameter(Position=0)]
+        [string]$Repository = '.'
+    )
+
+    throw 'Get-ChangedFiles scaffold created; implementation pending.'
+}
