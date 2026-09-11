3d2916729f5f3d950c99dfd275056bd1e598db63
Jason Feimster <jasonfeimster@gmail.com>
2026-09-11 06:04:37 -0400

    Add Get-WorkQueue script scaffold

---
+++ tools/Get-WorkQueue/scripts/Get-WorkQueue.ps1
@@ -0,0 +1,9 @@
+function Get-WorkQueue {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory, Position=0)]
+        [string]$Repository
+    )
+
+    throw 'Get-WorkQueue scaffold created; implementation pending.'
+}
