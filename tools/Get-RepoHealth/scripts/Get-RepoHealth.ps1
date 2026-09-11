25242a34406d50ffdc36fce25e224a67703fed44
Jason Feimster <jasonfeimster@gmail.com>
2026-09-11 06:03:30 -0400

    Add Get-RepoHealth script scaffold

---
+++ tools/Get-RepoHealth/scripts/Get-RepoHealth.ps1
@@ -0,0 +1,9 @@
+function Get-RepoHealth {
+    [CmdletBinding()]
+    param(
+        [Parameter(Position=0)]
+        [string]$Repository = '.'
+    )
+
+    throw 'Get-RepoHealth scaffold created; implementation pending.'
+}
