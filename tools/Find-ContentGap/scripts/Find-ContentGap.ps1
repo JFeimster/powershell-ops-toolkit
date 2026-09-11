eda74446bc6a6ccad2fd34add2f3fbf10966bf71
Jason Feimster <jasonfeimster@gmail.com>
2026-09-11 06:06:38 -0400

    Add Find-ContentGap script scaffold

---
+++ tools/Find-ContentGap/scripts/Find-ContentGap.ps1
@@ -0,0 +1,9 @@
+function Find-ContentGap {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory, Position=0)]
+        [string]$Topic
+    )
+
+    throw 'Find-ContentGap scaffold created; implementation pending.'
+}
