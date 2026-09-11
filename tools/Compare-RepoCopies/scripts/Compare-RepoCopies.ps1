332e36201aa7f798d71afa64bac6fe12b477bcb3
Jason Feimster <jasonfeimster@gmail.com>
2026-09-11 06:03:13 -0400

    Add Compare-RepoCopies script scaffold

---
+++ tools/Compare-RepoCopies/scripts/Compare-RepoCopies.ps1
@@ -0,0 +1,9 @@
+function Compare-RepoCopies {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory, Position=0)]
+        [string]$Repository
+    )
+
+    throw 'Compare-RepoCopies scaffold created; implementation pending.'
+}
