f045aa8b0766a17c24507fc026cff48eddf1bbc1
Jason Feimster <jasonfeimster@gmail.com>
2026-09-11 06:07:43 -0400

    Add Invoke-ProjectDoctor script scaffold

---
+++ tools/Invoke-ProjectDoctor/scripts/Invoke-ProjectDoctor.ps1
@@ -0,0 +1,9 @@
+function Invoke-ProjectDoctor {
+    [CmdletBinding()]
+    param(
+        [Parameter(Position=0)]
+        [string]$Repository = '.'
+    )
+
+    throw 'Invoke-ProjectDoctor scaffold created; implementation pending.'
+}
