f82ddb40d6d7a3a2eb8d76b414d63c46d43b2893
Jason Feimster <jasonfeimster@gmail.com>
2026-09-11 06:02:57 -0400

    Add Open-ProjectStack script scaffold

---
+++ tools/Open-ProjectStack/scripts/Open-ProjectStack.ps1
@@ -0,0 +1,9 @@
+function Open-ProjectStack {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory, Position=0)]
+        [string]$Repository
+    )
+
+    throw 'Open-ProjectStack scaffold created; implementation pending.'
+}
