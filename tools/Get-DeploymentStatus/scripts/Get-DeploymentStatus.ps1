723f1c8a4a96a59395e9e0e6988b3c2364f032a9
Jason Feimster <jasonfeimster@gmail.com>
2026-09-11 06:07:28 -0400

    Add Get-DeploymentStatus script scaffold

---
+++ tools/Get-DeploymentStatus/scripts/Get-DeploymentStatus.ps1
@@ -0,0 +1,9 @@
+function Get-DeploymentStatus {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory, Position=0)]
+        [string]$Repository
+    )
+
+    throw 'Get-DeploymentStatus scaffold created; implementation pending.'
+}
