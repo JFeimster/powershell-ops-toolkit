48c007a14d00c93da571985cebe53d4e15ab0eb5
Jason Feimster <jasonfeimster@gmail.com>
2026-09-11 06:02:00 -0400

    Add Start-Project script scaffold

---
+++ tools/Start-Project/scripts/Start-Project.ps1
@@ -0,0 +1,10 @@
+function Start-Project {
+    [CmdletBinding()]
+    param(
+        [Parameter(Position=0)]
+        [string]$Repository = '.',
+        [switch]$Open
+    )
+
+    throw 'Start-Project scaffold created; implementation pending.'
+}
