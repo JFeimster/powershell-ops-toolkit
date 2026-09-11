103a902334a68f97076add06c9c9cf17d29868c0
Jason Feimster <jasonfeimster@gmail.com>
2026-09-11 06:12:18 -0400

    Implement Open-GitHubRepo

--- tools/Open-GitHubRepo/scripts/Open-GitHubRepo.ps1
+++ tools/Open-GitHubRepo/scripts/Open-GitHubRepo.ps1
@@ -1,9 +1,24 @@
 function Open-GitHubRepo {
-    [CmdletBinding()]
+    [CmdletBinding(SupportsShouldProcess)]
     param(
         [Parameter(Mandatory, Position=0)]
-        [string]$Repository
+        [string]$Repository,
+        [switch]$PassThru
     )
 
-    throw 'Open-GitHubRepo scaffold created; implementation pending.'
+    $url = $null
+    if ($Repository -match '^https?://github\.com/[^/]+/[^/]+') {
+        $url = ($Repository -replace '\.git$', '')
+    } elseif (Test-Path -LiteralPath $Repository -PathType Container) {
+        $remote = (& git -C $Repository remote get-url origin 2>$null)
+        if ($remote) { $url = $remote -replace '^git@github\.com:', 'https://github.com/' -replace '\.git$', '' }
+    } elseif ($Repository -match '^[^/]+/[^/]+$') {
+        $url = "https://github.com/$Repository"
+    } elseif (Get-Command Find-LocalRepo -ErrorAction SilentlyContinue) {
+        $m = Find-LocalRepo $Repository | Select-Object -First 1
+        if ($m.GitRemote) { $url = $m.GitRemote -replace '^git@github\.com:', 'https://github.com/' -replace '\.git$', '' }
+    }
+    if (-not $url) { throw "Could not resolve a GitHub URL for '$Repository'." }
+    if ($PSCmdlet.ShouldProcess($url,'Open GitHub repository')) { Start-Process $url }
+    if ($PassThru) { $url }
 }
