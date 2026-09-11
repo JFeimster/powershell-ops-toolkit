e2222b5d27c43a1e5ae1873fc99f5e6f685beb8b
Jason Feimster <jasonfeimster@gmail.com>
2026-09-11 06:06:19 -0400

    Add Get-ContentInventory script scaffold

---
+++ tools/Get-ContentInventory/scripts/Get-ContentInventory.ps1
@@ -0,0 +1,9 @@
+function Get-ContentInventory {
+    [CmdletBinding()]
+    param(
+        [string[]]$Source,
+        [string]$Query
+    )
+
+    throw 'Get-ContentInventory scaffold created; implementation pending.'
+}
